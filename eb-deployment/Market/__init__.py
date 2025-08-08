71ae6fc8ffa1cd7d5fca7f050bad744d96dd2325
Syed Sajjad Askari <84753561+syedsajjadaskari@users.noreply.github.com>
2025-08-08 17:04:55 +0000

    add LICENSE, README, and initial application structure with user and item models

---
+++ eb-deployment/Market/__init__.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'e-commerce.db')
+
+# Updated SECRET_KEY configuration for production
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or os.urandom(12).hex()
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'LoginPage'
+login_manager.login_message_category = 'info'
+
+from Market import routes
